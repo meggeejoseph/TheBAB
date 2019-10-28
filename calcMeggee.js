c995b2d4b7f6806a98ee77904522c8d203a715bd
meggeejoseph <56272883+meggeejoseph@users.noreply.github.com>
2019-10-28 15:10:40 -0400

    add calculator for Meggee

---
+++ calcMeggee.js
@@ -0,0 +1,8 @@
+function calcBMI(){
+  w = weight.value  // read weight from element with id="weight"
+  h = height.value  // read height from element with id="height"
+  bmi = 703*w/(h*h)  // calculate the Body Mass Index
+  BMI.innerHTML =    // write into label element with id "BMI"
+      "Your BMI is "
+      +bmi.toFixed(1)  // round BMI to 1 decimal place
+}
