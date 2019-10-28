5f2f9afd058216736b7a2794091ddba89d651849
meggeejoseph <56272883+meggeejoseph@users.noreply.github.com>
2019-10-28 15:22:02 -0400

    changed to FPS demo

--- calcMeggee.js
+++ calcMeggee.js
@@ -1,8 +1,8 @@
-function calcBMI(){
-  w = weight.value  // read weight from element with id="weight"
-  h = height.value  // read height from element with id="height"
-  bmi = 703*w/(h*h)  // calculate the Body Mass Index
-  BMI.innerHTML =    // write into label element with id "BMI"
-      "Your BMI is "
-      +bmi.toFixed(1)  // round BMI to 1 decimal place
+function calcFPS(){
+  mph = mphTF.value  // read weight from element with id="weight"
+  fps = mph*5280/3600
+
+  fpsLabel.innerHTML =    // write into label element with id "BMI"
+      "Hi "+fullNameTF.value + ", your FPS is "
+      +fps.toFixed(1)  // round BMI to 1 decimal place
 }
